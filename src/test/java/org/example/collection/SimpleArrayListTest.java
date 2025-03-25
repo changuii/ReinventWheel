1e630201a2b86030d0a9d05e48d838e796f16551
changuii <rhljh7410@gmail.com>
2025-03-25 11:02:11 +0900

    JDBC 추가

--- src/test/java/org/example/collection/SimpleArrayListTest.java
+++ src/test/java/org/example/collection/SimpleArrayListTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 public class SimpleArrayListTest {
 
     @DisplayName("심플 리스트 테스트")
@@ -21,6 +24,19 @@ public class SimpleArrayListTest {
         assertThat(values.remove(0)).isEqualTo("first");
         assertThat(values.size()).isEqualTo(2);
 
+    }
+
+    @DisplayName("a")
+    @Test
+    void a() {
+        // given
+        List<Integer> integers = Collections.emptyList();
+        // when
+
+        assertThat(integers.stream()
+                .anyMatch(i -> i > 10)).isTrue();
+
+        // then
     }
 
 }
