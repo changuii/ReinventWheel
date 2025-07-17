97209da1e1392c2066e4b65e4a0ac6e6ac4c5ec8
changuii <rhljh7410@gmail.com>
2025-07-17 12:19:06 +0900

    test

---
+++ module1/src/test/java/com/example/Module1Test.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Module1Test {
+
+    @DisplayName("asd")
+    @Test
+    void asd() {
+        // given
+        System.out.println("Asd");
+        // when
+
+        // then
+    }
+}
